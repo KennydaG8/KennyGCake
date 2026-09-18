0d76cf0ce57927dc18a2dadfa9aa6712291d751f
KennydaG8 <orzroger@gmail.com>
2026-09-18 16:08:20 +0800

    feat: add tea and coffee to onsite payments

---
+++ group-buy-backend/seeds/production-onsite-flavors.sql
@@ -0,0 +1,8 @@
+INSERT INTO campaign_products (campaign_id, product_id, unit_price, display_order, active)
+VALUES
+  ('onsite-pay', 'sunmoonlake-black-tea', 85, 4, 1),
+  ('onsite-pay', 'gukeng-jiabishan-coffee', 85, 5, 1)
+ON CONFLICT(campaign_id, product_id) DO UPDATE SET
+  unit_price = excluded.unit_price,
+  display_order = excluded.display_order,
+  active = 1;
